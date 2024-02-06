e9cbf4773bcfef1c606ea7f4a861178bb7e89e79
Hirusha2000 <2000hirushaudara@gmail.com>
2024-02-06 10:28:40 +0530

    set up Express.js with basic configuration.

--- simpleCRUD/routes/index.js
+++ simpleCRUD/routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// Middleware
+app.use(bodyParser.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Welcome to the CRUD API with JWT authentication!');
 });
 
-module.exports = router;
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
