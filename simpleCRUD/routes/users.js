ab9bc76cb2cb8246d24aac79a97e4d1533f4c583
Hirusha2000 <2000hirushaudara@gmail.com>
2024-02-06 10:57:14 +0530

    added create position in users

--- simpleCRUD/routes/users.js
+++ simpleCRUD/routes/users.js
@@ -1,9 +1,35 @@
-var express = require('express');
-var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+const express = require('express');
+const router = express.Router();
+const pool = require('../db'); // Import the database connection module
+
+
+app.post('/users', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const result = await pool.query('INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *', [username, password]);
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
+// Read Users
+router.get('/', async (req, res) => {
+  // Implementation for reading users from the database
+});
+
+// Update User
+router.put('/:id', async (req, res) => {
+  // Implementation for updating a user in the database
+});
+
+// Delete User
+router.delete('/:id', async (req, res) => {
+  // Implementation for deleting a user from the database
 });
 
 module.exports = router;
